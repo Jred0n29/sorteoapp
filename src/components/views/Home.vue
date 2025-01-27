a26c8b1f68cd8b307e53508975060d38909a0e43
Jred0n29 <jredon439@gmail.com>
2025-01-27 14:56:34 -0500

    Cambios

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -19,10 +19,16 @@
                     </p>
                 </div>
                 <div class="mx-auto p-4 ">
-                    <button class="me-2 mb-2 animate-buttonheartbeat rounded-lg bg-red-500 px-5 py-2.5 text-sm font-semibold text-white text-center inline-flex items-center">
+                    <a href="#_" class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-500 bg-red-500 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-red-300 ring-offset-red-200 ease focus:outline-none transform animate-pulseUpDown">
+                    <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-20 group-hover:translate-x-0"></span>
+                    <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-20 group-hover:translate-x-0"></span>
+                    <span class="relative z-20 flex items-center text-sm">
                         Unete A Nuestro Grupo
-                       <img class="w-10 h-10 me-2 ml-2" src="/src/assets/wppcolor.svg" alt=""> 
-                    </button>
+                    </span>
+                    <img class="w-12 h-12 me-2 ml-2" src="/src/assets/wppcolor.svg" alt=""> 
+                </a>
+
+
                 </div>
             </div>
             
@@ -41,4 +47,23 @@
     </section>
 
     </template>
-    
+<style>
+    @keyframes pulseUpDown {
+        0% {
+            transform: scale(1);
+            box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+        }
+        50% {
+            transform: scale(1.05);
+            box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.2);
+        }
+        100% {
+            transform: scale(1);
+            box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+        }
+    }
+
+    .animate-pulseUpDown {
+        animation: pulseUpDown 2s infinite ease-in-out;
+    }
+</style>
